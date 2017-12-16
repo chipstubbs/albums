34224ddf172ed29e450ee9b6250d7aeaaad0aead
= <=>
2017-12-15 22:15:07 -0500

    49

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,70 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+// {album} was just props and inside the function props.album.title | props.album.artist | etc 
+// was destructed below
+const AlbumDetail = ({album}) => {
+    const { title, artist, thumbnail_image, image } = album;
+    // destructure styles
+    const { 
+        thumbnailStyle, 
+        headerContentStyle, 
+        thumbContainer ,
+        headerTitle,
+        albumImage
+    } = styles;
+
     return (
-        <View>
-            <Text>{props.album.title}</Text>
-        </View>
+        <Card>
+            <CardSection>
+                <View style={thumbContainer}>
+                    <Image 
+                        style={thumbnailStyle} 
+                        source={{ uri: thumbnail_image }} 
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTitle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>  
+            </CardSection>
+
+            <CardSection>
+                <Image 
+                    source={{ uri: image }}
+                    style={albumImage}
+                />
+            </CardSection>
+        </Card>
     );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTitle: {
+        fontSize: 20
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    albumImage: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
+
 };
 
 export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -8,7 +8,9 @@ class AlbumList extends Component {
 
     componentWillMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => this.setState({ albums: response.data }));
+            .then(
+                response => this.setState({ albums: response.data })
+            );
     }
 
     renderAlbums() {
@@ -21,9 +23,9 @@ class AlbumList extends Component {
     render() {
         console.log(this.state.albums);
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         );
     }
     
